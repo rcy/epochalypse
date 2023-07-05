a44bc05ed19883fea8634d97ae8c18f0e306ab25
Ryan Yeske <ryan@ryanyeske.com>
2023-07-05 13:17:14 -0700

    dump env

--- daysleft.sh
+++ daysleft.sh
@@ -5,6 +5,8 @@ set -euxo pipefail
 jq --version
 curl --version
 
+env
+
 let X=(`date +%s -d 2038-01-19T03:14:07Z` - `date +%s`)/86400
 
 URL=$(curl --silent "https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${X}" | jq -r .items[0].link)
