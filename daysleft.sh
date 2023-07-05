4313fcb3a70da319c097b4dbbe1211692d293428
Ryan Yeske <ryan@ryanyeske.com>
2023-07-05 13:11:40 -0700

    Google search the days left and tweet the first result

--- daysleft.sh
+++ daysleft.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+jq --version
+curl --version
+
 let X=(`date +%s -d 2038-01-19T03:14:07Z` - `date +%s`)/86400
 
-echo The world will end in $X days. | curl $FLAPPER_URL -d@-
+URL=$(curl --silent "https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${X}" | jq -r .items[0].link)
+
+echo -e "The world will end in ${X} days ${URL}" | curl -d@- ${FLAPPER_URL}
