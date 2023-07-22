a8003eb55568ac3e522a82d99ed0cd788c8e8343
Ryan Yeske <ryan@ryanyeske.com>
2023-07-21 20:22:17 -0700

    Prevent .gov sites

--- daysleft.sh
+++ daysleft.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 
 let X=(`date +%s -d 2038-01-19T03:14:07Z` - `date +%s`)/86400
 
-URL=$(curl --silent "https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${X}" | jq -r .items[0].link)
+URL=$(curl --silent "https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=-site:.gov+${X}" | jq -r .items[0].link)
 
 echo -e "The world will end in ${X} days ${URL}" | curl -d@- ${FLAPPER_URL}
+
