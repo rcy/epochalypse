46e8f2ece6a5c95e955ccc86eb1f95d1a126632b
Ryan Yeske <ryan@ryanyeske.com>
2023-08-24 06:26:56 -0700

    Search watches

--- daysleft.sh
+++ daysleft.sh
@@ -4,7 +4,9 @@ set -euxo pipefail
 
 let X=(`date +%s -d 2038-01-19T03:14:07Z` - `date +%s`)/86400
 
-URL=$(curl --silent "https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=-site:.gov+${X}" | jq -r .items[0].link)
+Q="${X} watch"
+
+URL=$(curl --silent "https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${Q}" | jq -r .items[0].link)
 
 echo -e "The world will end in ${X} days ${URL}" | curl -d@- ${FLAPPER_URL} | tee result
 grep 'sent tweet' result
