4ac4ab96405d45d419de6042d58cfa1a9a950717
Ryan Yeske <ryan@ryanyeske.com>
2023-08-24 09:33:55 -0700

    Escape the space

--- daysleft.sh
+++ daysleft.sh
@@ -4,9 +4,10 @@ set -euxo pipefail
 
 let X=(`date +%s -d 2038-01-19T03:14:07Z` - `date +%s`)/86400
 
-Q="${X} watch"
+Q="${X}+watch"
 
-URL=$(curl --silent "https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${Q}" | jq -r .items[0].link)
+URL=$(curl --silent "https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${Q}" | tee result | jq -r .items[0].link)
+cat result
 
 echo -e "The world will end in ${X} days ${URL}" | curl -d@- ${FLAPPER_URL} | tee result
 grep 'sent tweet' result
